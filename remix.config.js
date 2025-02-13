7d00b6a4a961bd200e5d8639c44c15603071d8d5
Abubakar <abubakaribro1@gmail.com>
2025-02-13 18:22:28 +0100

    revert document

--- remix.config.js
+++ remix.config.js
@@ -1,47 +1,20 @@
-// import flatRoutes from 'remix-flat-routes'
-import { flatRoutes } from 'remix-flat-routes';
-
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   tailwind: true,
   postcss: true,
 
   ignoredRouteFiles: ["**/.*"],
-  routes: async (defineRoutes) => {
-    return flatRoutes('routes', defineRoutes);
-  },
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
 
   browserNodeBuiltinsPolyfill: {
     modules: {
-      // Core modules
       punycode: true,
-      http: true,
-      https: true,
-      url: true,
-      buffer: true,
-      stream: true,
-      crypto: true,
-      
-      // Additional required modules
-      string_decoder: true,
-      assert: true,
-      zlib: true,
-      util: true,
-      events: true,
-      
-      // Common dependencies
-      os: true,
-      path: true,
-      fs: true,
-      vm: true,
     },
   },
-  
-  serverModuleFormat: "esm",
-  serverPlatform: "node",
-  serverMinify: false,
 
-  // Bundle CommonJS modules
   serverDependenciesToBundle: [
     "@phosphor-icons/react",
     "@icons-pack/react-simple-icons",
